b84b2a081328c752ff01175902b082514cdb4808
TatsukiHikida <c0a201284c@edu.teu.ac.jp>
2022-12-05 04:33:43 +0000

    並列化

--- alert.py
+++ alert.py
@@ -2,58 +2,29 @@ import requests
 import json
 from datetime import datetime
 from kubernetes import client, config
+import time
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
-# get current time
-now = datetime.now()
-
-# interval μs
-interval = 600000000
-
-# duration limit μs
-duration_lim = 5000
-
-# now(end)～$interval μs ago(start)
-ts_end = str(datetime.timestamp(now)).replace('.', '')
-ts_start = str(datetime.timestamp(now) - interval).replace('.', '')
-
-# jaeger port
-port = 16686
-
-# serch limit
-limit = 20
-
-# get kubernetes svc
-svc_ns_list = []
-namespace = "sock-shop"
-config.load_kube_config()
-v1 = client.CoreV1Api()
-
-svcs = v1.list_namespaced_service(namespace)
-for i in svcs.items:
-    svc_ns_list.append( i.metadata.name + "." + namespace )
-
-for svc_ns in svc_ns_list: 
-    # jaeger ui url
-    URL = ( "http://localhost:" + str(port)
-            + "/jaeger/api/traces"
-            + "?end=" + ts_end 
-            + "&limit=" + str(limit)
-            + "&lookback=1h&maxDuration&minDuration"
-            + "&service=" + svc_ns
-            + "&start="+ ts_start )
-
+def slack_webhook(metrics_dict, svc):
     # slack webhook
-    webhook_url = ''
+    webhook_url = 'https://hooks.slack.com/services/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'
     channel = "#general"
     username = "Sock-shop-Alert"
 
-    # get jaeger jason
-    r = requests.get(URL)
-    data = r.json()
+    duration_lim = 1
 
-    # get duration for each operationName
+    # average duration for each operationName
+    for key in metrics_dict:
+        ave = sum(metrics_dict[key]) / len(metrics_dict[key])
+        if ave >= duration_lim:
+            text = f"serviecName:{svc}\noperationName:{key}\nexceed {duration_lim}μs!"
+            webhook_data = { "channel": channel, "username": username, "text": text, "icon_emoji": ":ghost:" }
+            print(webhook_data.items())
+            requests.post(webhook_url, data=json.dumps(webhook_data))
+
+
+def get_metrics(data, limit, svc):
     name_duration = {}
-
     for i in range(limit):
         try:
             traceID = data['data'][i]
@@ -69,11 +40,53 @@ for svc_ns in svc_ns_list:
             else:
                 name_duration[operationName].append(duration)
 
-    # average duration for each operationName
-    for key in name_duration:
-        ave = sum(name_duration[key]) / len(name_duration[key])
-        if ave >= duration_lim:
-            text = f"serviecName:{svc_ns}\noperationName:{key}\nexceed {duration_lim}μs!"
-            webhook_data = { "channel": channel, "username": username, "text": text, "icon_emoji": ":ghost:" }
-            print(webhook_data.items())
-            requests.post(webhook_url, data=json.dumps(webhook_data))
+    slack_webhook(name_duration, svc)
+
+def main():
+    # get current time
+    now = datetime.now()
+
+    # interval ms
+    interval = 3600000
+
+    # duration limit μs
+    duration_lim = 5000
+
+    # now(end)～$interval μs ago(start)
+    ts_end = str(datetime.timestamp(now)).replace('.', '')
+    ts_start = str(datetime.timestamp(now) - interval).replace('.', '')
+
+    # jaeger port
+    port = 16686
+
+    # serch limit
+    limit = 20
+
+    # get kubernetes svc
+    svc_ns_list = []
+    namespace = "sock-shop"
+    config.load_kube_config()
+    v1 = client.CoreV1Api()
+
+    svcs = v1.list_namespaced_service(namespace)
+    for i in svcs.items:
+        svc_ns_list.append( i.metadata.name + "." + namespace )
+
+    for svc_ns in svc_ns_list: 
+        # jaeger ui url
+        URL = ( "http://localhost:" + str(port)
+                + "/jaeger/api/traces"
+                + "?end=" + ts_end 
+                + "&limit=" + str(limit)
+                + "&lookback=1h&maxDuration&minDuration"
+                + "&service=" + svc_ns
+                + "&start="+ ts_start )
+
+        # get jaeger jason
+        r = requests.get(URL)
+        data = r.json()
+
+        get_metrics(data, limit, svc_ns)
+
+if __name__ == "__main__":
+    main()
