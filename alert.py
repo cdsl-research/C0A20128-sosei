78aa9b54e32d89a6c0c9334c94ed71296b10f3bd
TatsukiHikida <c0a201284c@edu.teu.ac.jp>
2022-12-19 00:40:14 +0000

    new

--- alert.py
+++ alert.py
@@ -5,43 +5,6 @@ from kubernetes import client, config
 from concurrent.futures import ProcessPoolExecutor
 import os
 
-def slack_webhook(metrics_dict, svc):
-    # slack webhook
-    webhook_url = os.environ['SLACK_WEBHOOK']
-    channel = "#general"
-    username = "Sock-shop-Alert"
-
-    # μs
-    duration_lim = 3000000
-
-    # average duration for each operationName
-    for key in metrics_dict:
-        ave = sum(metrics_dict[key]) / len(metrics_dict[key])
-        if ave >= duration_lim:
-            text = f"serviecName:{svc}\noperationName:{key}\nexceed 3000 ms!\n{datetime.now()}"
-            webhook_data = { "channel": channel, "username": username, "text": text, "icon_emoji": ":ghost:" }
-            print(webhook_data.items())
-            requests.post(webhook_url, data=json.dumps(webhook_data))
-
-def get_metrics(data, limit, svc):
-    name_duration = {}
-    for i in range(limit):
-        try:
-            traceID = data['data'][i]
-        except IndexError: 
-            print(f"Number of traceID under limit")
-            break
-
-        for j in range(len(traceID['spans'])):
-            operationName = traceID['spans'][j]['operationName']
-            duration = traceID['spans'][j]['duration']
-            if (operationName not in name_duration ):
-                name_duration[operationName] = [duration]
-            else:
-                name_duration[operationName].append(duration)
-
-    slack_webhook(name_duration, svc)
-
 def main():
     # get current time
     now = datetime.now()
